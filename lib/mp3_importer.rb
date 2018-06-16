7b25f2e2c3f959f033d7fe09833f72974f64da2c
Mark Lawrence <lawrence.ml@gmail.com>
2018-06-16 00:06:12 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    files = Dir.entries(@path)
+    files.select { |file| file.end_with?('mp3') }
+  end
+
+  def import
+    self.files.each { |file| Song.new_by_filename(file) }
+  end
+end
